938bad983176c031f071153fcd972614881d6590
geofholbrook <geof.holbrook@gmail.com>
2017-01-16 17:10:22 -0500

    stepwise GA implemented (canned fitness function)

---
+++ scripts/geof.js
@@ -0,0 +1,16 @@
+// geof.js
+// uses jquery
+
+Gf = {
+
+	// deep copy
+	copy: function (obj) {
+		var newobj = {};
+		jQuery.extend( true, newobj, obj );
+		return newobj;
+	},
+
+	rrnd: function (min, max) {
+		return Math.floor (Math.random() * (max - min + 1)) + min;
+	}
+}
--- scripts/js-darwin.js
+++ scripts/js-darwin.js
@@ -1,8 +1,6 @@
 // js-darwin.js
 
-function rrnd (min, max) {
-	return Math.floor (Math.random() * (max - min + 1)) + min;
-}
+// uses geof.js
 
 function range_modulo (num, range) {
 	var min = range[0];
@@ -17,17 +15,24 @@ function Specimen (len, range) {
 	this.len = len;
 
 	this.nucl = [];
+	this.randomize();
+
 	this.fitness = -1;
 	// -1 is a placeholder (not evaluated yet)
 	// lower fitness value means better specimen
 	// 0 means perfect
+
+	this.population = null;
+	this.engine = null;
 }
 
 Specimen.prototype.mutate = function() {
-	var spot = rrnd(0, this.len - 1);
-	var delta = rrnd(0,1) * 2 - 1;
+	var spot = Gf.rrnd(0, this.len - 1);
+	var delta = Gf.rrnd(0,1) * 2 - 1;
 
 	this.nucl[spot] = range_modulo(this.nucl[spot] + delta, this.range);
+
+	return this;
 }
 
 Specimen.prototype.randomize = function() {
@@ -35,38 +40,102 @@ Specimen.prototype.randomize = function() {
 	this.nucl=[];
 
 	for (i=0; i<this.len; i++) {
-		this.nucl.push( rrnd (this.range[0], this.range[1]));
+		this.nucl.push( Gf.rrnd (this.range[0], this.range[1]));
 	}
 
+	this.evaluate();
 }
 
 Specimen.prototype.evaluate = function() {
-	fit = 0;
-	nucl = this.nucl;
 
-	for (i in nucl) {
-		if ([1,3,6,8,10].indexOf(this.nucl[i] % 12) == -1)
-			{ fit += 0.1; }
+	var x = this.nucl; 
+	if (this.engine) {
+		this.fitness = this.engine.eval_func(x);
 	}
+	else
+		{ this.fitness = -1; }
+	
+	return this;
+}
 
-	this.fitness = fit;
-	return fit;
-	}
-
-function Population (capacity, fecundity, model) {
+function Population (capacity, litter_size, model) {
 	this.capacity = capacity;
-	this.fecundity = fecundity;
+	this.litter_size = litter_size;
 	this.model = model;
 
 	this.specimens = [];
-
-	for (k=0;k<capacity;k++) {
-		this.specimens.push( 
-			new Specimen( model.len, model.range ) )
-	}
+	this.randomize();
+	this.engine = null;
 }
 
-Population.prototype.iterate = function() {
+Population.prototype.randomize = function() {
+	this.specimens = [];
+
+	for (k=0;k<this.capacity;k++) {
+		this.specimens.push( 
+			new Specimen( this.model.len, this.model.range ) )
+	}
+
+	// doesn't work, because 'this' returns the window object.
+	// this.specimens.map( function(spec) { spec.population = this } );
+}
+
+Population.prototype.best = function() {
+	// assumes population is always kept sorted
+	return this.specimens[0];
+}
+
+
+
+function Engine (population) {
+	this.population = population;
+
+	this.eval_func = function (x) {
+
+			fit = 0;
+
+			for (i in x) {
+				if ([1,3,6,8,10].indexOf(x[i] % 12) == -1)
+					{ fit += 0.1; }
+			}
+			return fit;
+	}
+
+	this.generation = 0;
+}
+
+Engine.prototype.iterate = function() {
+
+	console.log("iterating.");
+
+	var new_copies = [];
+
+	for (i in this.population.specimens) {
+		var spec = this.population.specimens[i];
+
+		for (k=0;k<this.population.litter_size;k++) {
+		
+			// copy, mutate, evaluate and add new specimen
+			new_copies.push( Gf.copy(spec).mutate().evaluate() );
+		}
+	}
+
+	this.population.specimens = this.population.specimens.concat(new_copies);
+	this.population.specimens = this.population.specimens.sort(function(a,b) { return a.fitness - b.fitness } );
+	this.population.specimens.splice( this.population.capacity );
+
+
+	return this.population.best(); // most fit specimen
+}
+
+function link_GA_objects (eng) {
+
+	eng.population.engine = eng;
+
+	for (i in eng.population.specimens) {
+		eng.population.specimens[i].population = eng.population;
+		eng.population.specimens[i].engine = eng;
+	}
 
 }
 
